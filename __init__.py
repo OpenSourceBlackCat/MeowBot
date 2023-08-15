5d36b3f5f7f60db06a69fc697f7d387850d870cc
AmeyaGurjar <amey992004@gmail.com>
2023-08-15 21:40:59 +0530

    Rebasing

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+from os import path as opath, listdir
+from sys import path as spath
+for path in listdir("."):
+	if opath.isdir(path):
+		spath.append(opath.abspath(path))
 from AmeyBot import mainGui
 from os import system
 from colorama import init
