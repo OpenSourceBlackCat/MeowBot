0d20556588218fa5a823c06cb06f38cb9d386671
OpenSourceBlackCat <amey992004@gmail.com>
2023-10-16 17:07:54 +0530

    Rebase

--- app/MeowBotSettings.py
+++ app/MeowBotSettings.py
@@ -54,7 +54,7 @@ def configValidator(meowBotConfig, configFunction ,optionConfig, inputString):
 def configCheck(meowBotConfig):
     jsonFetch()
     for i in range(len(configBotFunction)):
-	print(meowBotConfig, configBotFunction[i], optionBotConfig[i], inputString)
+        print(meowBotConfig, configBotFunction[i], optionBotConfig[i], inputString)
         configValidator(meowBotConfig=meowBotConfig, configFunction=configBotFunction[i], optionConfig=optionBotConfig[i], inputString=inputBotString[i])
     
 def configRun():
