3ac3d8d5409be8b0004bed190ed258d328f11f94
OpenSourceBlackCat <amey992004@gmail.com>
2023-12-25 17:39:15 +0530

    Debugging

--- app/MeowBot.py
+++ app/MeowBot.py
@@ -138,7 +138,7 @@ try:
                 id=vidlink
             )
             livechatid = livechatrequest.execute()
-            print(livechatid)
+            print(livechatid["liveStreamingDetails"])
             livechatid = livechatid["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
             botNameUpper = botName.upper()
             insert_comment(messagetext=f"{botNameUpper} BOT IS RUNNING SUCCESSFULLY.")
